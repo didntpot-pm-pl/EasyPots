6dbfb70f10f0d17e59962cb12358223c840e1211
Peppa <79522460+DidntPot@users.noreply.github.com>
2021-12-12 23:53:58 -0800

    Init.

---
+++ src/DidntPot/EasyPots/EventListener.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace DidntPot\EasyPots;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\item\ItemFactory;
+use pocketmine\item\ItemIds;
+
+class EventListener implements Listener
+{
+    /**
+     * @var Loader
+     */
+    public Loader $plugin;
+
+    /**
+     * @param Loader $plugin
+     */
+    public function __construct(Loader $plugin)
+    {
+        $this->plugin = $plugin;
+    }
+
+    /**
+     * @param PlayerInteractEvent $ev
+     */
+    public function onInteract(PlayerInteractEvent $ev)
+    {
+        $player = $ev->getPlayer();
+        $action = $ev->getAction();
+        $item = $player->getInventory()->getItemInHand();
+
+        if($action === $ev::RIGHT_CLICK_BLOCK)
+        {
+            if($item->getId() === ItemIds::SPLASH_POTION)
+            {
+                $player->getInventory()->getItemInHand()->onClickAir($player, $player->getDirectionVector());
+
+                if(!$player->isCreative())
+                {
+                    $player->getInventory()->setItem($player->getInventory()->getHeldItemIndex(), ItemFactory::getInstance()->getItem(0));
+                }
+            }
+        }
+    }
+}
